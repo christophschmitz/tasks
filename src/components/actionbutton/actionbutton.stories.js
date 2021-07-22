7b889954869b5a6f364cf789bef3abd35a33b3f3
christophschmitz <c.schmitz@outlook.de>
2021-07-22 11:25:20 +0200

    Add basic folder structure

---
+++ src/components/actionbutton/actionbutton.stories.js
@@ -0,0 +1,9 @@
+import "./actionbutton.css";
+import actionbutton from "./actionbutton.html";
+
+export default {
+  title: "Components/actionbutton",
+  parameters: { layout: "fullscreen" },
+};
+
+export const Button = () => actionbutton;
