8c4c3ac6641293fe9a31d8b840d41eba41eb1128
christophschmitz <c.schmitz@outlook.de>
2021-07-26 16:14:35 +0200

    Add Notoficationbutton

--- src/components/iconButton/iconButton.stories.js
+++ src/components/iconButton/iconButton.stories.js
@@ -1,9 +1,11 @@
 import "./iconButton.css";
 import iconButtonMenu from "./iconButton-menu.html";
+import iconButtonNotification from "./iconButton-notification.html";
 
 export default {
   title: "Components/IconButton",
   parameters: { layout: "center" },
 };
 
-export const Button = () => iconButtonMenu;
+export const MenuButton = () => iconButtonMenu;
+export const BellButton = () => iconButtonNotification;
